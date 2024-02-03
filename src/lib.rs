d6fc4729ed98af1c6697bb4771d498ffc856ad5b
MartBent <martbent99@gmail.com>
2024-02-03 16:44:03 +0100

    finished game

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use alloc::boxed::Box;
 use alloc::format;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+
 pub struct Cell {
     red: u8,
     green: u8,
@@ -60,6 +61,7 @@ pub struct SnakeGame {
     snake: Vec<(u32, u32)>,
     current_food: (u32, u32),
     current_direction: Direction,
+    finished: bool,
     provider: Box<dyn SnakeProvider>,
 }
 
@@ -73,6 +75,24 @@ impl SnakeGame {
     }
 
     pub fn set_direction(&mut self, direction: Direction) {
+        if direction == Direction::Up && self.current_direction != Direction::Down {
+            self.current_direction = direction;
+        }
+
+        else if direction == Direction::Down && self.current_direction != Direction::Up {
+            self.current_direction = direction;
+        }
+
+        else if direction == Direction::Left && self.current_direction != Direction::Right {
+            self.current_direction = direction;
+        }
+
+        else if direction == Direction::Right && self.current_direction != Direction::Left {
+            self.current_direction = direction;
+        }
+    }
+
+    pub fn set_direction_unchecked(&mut self, direction: Direction) {
         self.current_direction = direction;
     }
 
@@ -86,9 +106,6 @@ impl SnakeGame {
     }
 
     pub fn tick(&mut self) {
-
-        let msg = format!("Random: {}", self.provider.provide_random_number(32));
-        self.provider.debug_log(msg.as_str());
 
         let mut next = self.cells.clone();
 
@@ -109,60 +126,79 @@ impl SnakeGame {
             blue: 0,
         };
 
-        // move the snake
-        let current_direction = self.current_direction;
+        // set new points and verify boundaries
+        let mut new_snake: Vec<(u32, u32)> = vec![];
 
-        let (row_offset, column_offset): (i32, i32) = match current_direction {
+        // move the snake
+        let (row_offset, column_offset): (i32, i32) = match self.current_direction {
             Direction::Up => (-1, 0),
             Direction::Down => (1, 0),
             Direction::Left => (0, -1),
             Direction::Right => (0, 1),
         };
 
-        // set new points and verify boundaries
-        let mut new_snake: Vec<(u32, u32)> = vec![];
-        let mut new_segment: Option<(u32, u32)> = None;
+        // get the new head location
+        let snake_head = self.snake.first().unwrap();
+        let row_offseted = snake_head.0 as i32 + row_offset;
+        let column_offseted = snake_head.1 as i32 + column_offset;
 
-        for (index, &(row, column)) in &mut self.snake.iter().enumerate() {
-            if index == 0 {
-                if row == self.current_food.0 && column == self.current_food.1 {
-                    new_segment = Some(*self.snake.last().unwrap());
+        let new_row: u32;
+        let new_column: u32;
+
+        if row_offseted == self.size as i32 {
+            new_row = 0;
+        } else if row_offseted == -1 {
+            new_row = self.size - 1;
+        } else {
+            new_row = row_offseted as u32;
+        }
+
+        if column_offseted == self.size as i32 {
+            new_column = 0;
+        } else if column_offseted == -1 {
+            new_column = self.size - 1;
+        } else {
+            new_column = column_offseted as u32;
+        }
+
+        new_snake.push((new_row, new_column));
+
+        for (index, &segment) in &mut self.snake.iter().enumerate() {
+
+            // check if any segments collide with the new head
+            if segment == *new_snake.first().unwrap() {
+                self.finished = true;
+                break;
+            }
+
+            // only add the last segment if some food was eaten
+            if index == self.snake.len() - 1 {
+                if snake_head == &self.current_food {
+                    let msg = format!("Ate some food! new length {}", new_snake.len());
+                    self.provider.debug_log(msg.as_str());
+
+                    // keep the tail on the new snake when food was eaten
+                    new_snake.push(segment);
+
+                    // create new food
+                    let mut new_food = (self.provider.provide_random_number(self.size), self.provider.provide_random_number(self.size));
+
+                    // if the new food location is invalid, retry. This will not work properly when the snake is really long
+                    while new_snake.iter().all(|segment| *segment == new_food) {
+                        new_food =  (self.provider.provide_random_number(self.size), self.provider.provide_random_number(self.size));
+                    }
+
+                    self.current_food = new_food;
+                    
                 }
-            }
-
-            let row_offseted = row as i32 + row_offset;
-            let column_offseted = column as i32 + column_offset;
-
-            let new_row: u32;
-            let new_column: u32;
-
-            if row_offseted == self.size as i32 {
-                new_row = 0;
-            } else if row_offseted == -1 {
-                new_row = self.size - 1;
             } else {
-                new_row = row_offseted as u32;
+                // concatinate the old snake with the new head
+                new_snake.push(segment);
             }
-
-            if column_offseted == self.size as i32 {
-                new_column = 0;
-            } else if column_offseted == -1 {
-                new_column = self.size - 1;
-            } else {
-                new_column = column_offseted as u32;
-            }
-
-            new_snake.push((new_row, new_column));
         }
 
-        if let Some(segment) = new_segment {
-            let msg = format!("Ate some food! new length {}", self.snake.len());
-            self.provider.debug_log(msg.as_str());
-            self.snake.push(segment);
-        }
-
-        // draw the snake
-        for (row, column) in &self.snake {
+        // draw the new snake
+        for (row, column) in &new_snake {
             let index = self.get_cell_index(*row, *column);
             next[index] = Cell {
                 red: 0,
@@ -171,8 +207,32 @@ impl SnakeGame {
             };
         }
 
-        self.snake = new_snake;
-        self.cells = next;
+        if !self.finished {
+            self.snake = new_snake;
+            self.cells = next;
+        } else {
+            self.provider.debug_log("Game over! restarting...");
+
+            // reset game
+            self.current_direction = Direction::Up;
+
+            self.snake.clear();
+            self.snake.push((self.size/2,self.size/2));
+
+            self.cells.clear();
+            for _ in 0..self.size * self.size {
+                self.cells.push(Cell {
+                    red: 0,
+                    green: 0,
+                    blue: 0,
+                });
+            }
+
+            let food = (self.provider.provide_random_number(self.size), self.provider.provide_random_number(self.size));
+            self.current_food = food;
+            self.finished = false;
+        }
+
     }
 
     pub fn new(size: u32, provider: Box<dyn SnakeProvider>) -> SnakeGame {
@@ -186,12 +246,15 @@ impl SnakeGame {
             });
         }
 
+        let food = (provider.provide_random_number(size), provider.provide_random_number(size));
+
         SnakeGame {
             size,
             cells,
             snake: vec![(size / 2, size / 2)],
-            current_food: (0, 0),
+            current_food: food,
             current_direction: Direction::Up,
+            finished: false,
             provider
         }
     }
